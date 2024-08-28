893f551b7a3b76748bdc80b05c60a1ce3cd84308
co11aps <143988142+co11aps@users.noreply.github.com>
2024-08-28 17:02:08 +0800

    calendar off

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import { Calendar } from "./components/Calendar/Calendar";
+// import { Calendar } from "./components/Calendar/Calendar";
 
 function App() {
   return (
     <>
       <h1>Water Tracker</h1>
-      <Calendar />
+      {/* <Calendar /> */}
     </>
   );
 }
