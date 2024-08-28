1c72358ee01d7b19baafe00bdeb576f704cbbcfb
co11aps <143988142+co11aps@users.noreply.github.com>
2024-08-28 23:35:17 +0800

    calendar styles update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import ColorSchemeToggle from "./components/ColorSchemeToggler/ColorSchemeToggler";
-// import Calendar from "./components/Calendar/Calendar";
+import Calendar from "./components/Calendar/Calendar";
 
 function App() {
   return (
     <>
       <ColorSchemeToggle />
       <h1>Water Tracker</h1>
-      {/* <Calendar /> */}
+      <Calendar />
     </>
   );
 }
