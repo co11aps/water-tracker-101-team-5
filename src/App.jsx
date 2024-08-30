971f1623a3d27314b2aae90c4377527745ab50b3
anastasiyaaa0 <anastasiiiia98@gmail.com>
2024-08-31 00:08:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import ColorSchemeToggle from "./components/ColorSchemeToggler/ColorSchemeToggler";
+import Calendar from "./components/Calendar/Calendar";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import SigninPage from "./pages/SigninPage/SigninPage";
 import SignupPage from "./pages/SignupPage/SignupPage";
-import HomePage from "./pages/HomePage/HomePage";
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<WelcomePage />} />
         <Route
           path="/"
           element={
@@ -20,7 +22,7 @@ function App() {
               <div>
                 <Link to="/signup">Sign Up</Link>
               </div>
-              <HomePage />
+              <Calendar />
             </>
           }
         />
