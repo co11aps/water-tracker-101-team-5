ff920ad9444d9096417167664ab07cd35556bcb8
Kris-tina30 <kristina.pichkar.15@gmail.com>
2024-08-30 19:40:15 +0200

    logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,20 @@
 import React from "react";
-import "./header.module.css";
+import css from "./header.module.css";
+import Icon from "../Icon/Icon";
+import logo from "../../images/logo/icon-logo.svg";
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <header className="header">
+    <div className={css.headerContainer}>
+      <header className={css.header}>
         <a href="" lang="en" className="logo">
           Tracker of water
-          <svg width="102" height="48">
-            <use href="/src/images/logo/icon-logo.svg"></use>
-          </svg>
+          <img src={logo} alt="logo" />
         </a>
         <nav className="header-nav">
           <button className="header-button" type="button">
             Sign in
-            <svg className="button-svg">
-              <use href="/src/images/icons/icons.svg#icon-user"></use>
-            </svg>
+            {/* <Icon id={}/> */}
           </button>
         </nav>
       </header>
