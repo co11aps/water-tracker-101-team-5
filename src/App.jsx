4295c4865b9b58fa660a73c413eae7b5ff1949e3
co11aps <143988142+co11aps@users.noreply.github.com>
2024-08-30 18:32:56 +0800

    Icon component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import "./App.css";
 import ColorSchemeToggle from "./components/ColorSchemeToggler/ColorSchemeToggler";
 import Calendar from "./components/Calendar/Calendar";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import SigninPage from './pages/SigninPage/SigninPage';
-import SignupPage from './pages/SignupPage/SignupPage';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import SigninPage from "./pages/SigninPage/SigninPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
 
 function App() {
   return (
-     <Router>
-      <Routes> 
-        <Route path="/signin" element={<SigninPage />} /> 
-        <Route path="/signup" element={<SignupPage />} /> 
-        <Route path="/" element={
-          <>
-            <ColorSchemeToggle />
-            <h1>Water Tracker</h1>
-            <div>
-              <Link to="/signup">Sign Up</Link> 
-            </div>
-            <Calendar />
-          </>
-        } />
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <ColorSchemeToggle />
+              <h1>Water Tracker</h1>
+              <div>
+                <Link to="/signup">Sign Up</Link>
+              </div>
+              <Calendar />
+            </>
+          }
+        />
       </Routes>
     </Router>
   );
