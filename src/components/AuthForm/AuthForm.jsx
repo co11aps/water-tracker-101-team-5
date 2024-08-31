853ee5e9b3507d11c69859f3155a8aa0f1a1ee6e
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-08-31 20:24:00 +0300

    Formik for AuthForm

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,64 +1,81 @@
 import { useNavigate } from "react-router-dom";
-import css from "./AuthForm.module.css";
 import { useDispatch } from "react-redux";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import css from "./AuthForm.module.css";
 import { logIn, register } from "../../redux/auth/operations";
 
 const AuthForm = ({ isSignup }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string().min(6, "Password must be at least 6 characters").required("Required"),
+    ...(isSignup && {
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password"), null], "Passwords must match")
+        .required("Required"),
+    }),
+  });
 
-    if (!isSignup) {
-      dispatch(
-        logIn({
-          email: form.elements.email.value,
-          password: form.elements.password.value,
-        })
-      )
+  const handleSubmit = async (values, { setSubmitting }) => {
+    if (isSignup) {
+      dispatch(register({ email: values.email, password: values.password }))
         .unwrap()
         .then(() => {
-          console.log("login success");
+          console.log("Register success");
           navigate("/home");
         })
         .catch((err) => {
-          console.log("login error", err);
+          console.log("Register error", err);
         });
-      form.reset();
+    } else {
+      dispatch(logIn({ email: values.email, password: values.password }))
+        .unwrap()
+        .then(() => {
+          console.log("Login success");
+          navigate("/home");
+        })
+        .catch((err) => {
+          console.log("Login error", err);
+        });
     }
-
-    dispatch(
-      register({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    )
-      .unwrap()
-      .then(() => {
-        console.log("Register success");
-        navigate("/home");
-      })
-      .catch((err) => {
-        console.log("Register error", err);
-      });
-    form.reset();
+    setSubmitting(false);
   };
 
   return (
     <div className={css.authFormContainer}>
-      <form onSubmit={handleSubmit}>
-        <div className={css.formGroup}>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" required />
-        </div>
-        <div className={css.formGroup}>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" required />
-        </div>
-        <button type="submit">{isSignup ? "Sign Up" : "Sign In"}</button>
-      </form>
+      <Formik
+        initialValues={{ email: "", password: "", confirmPassword: "" }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div className={css.formGroup}>
+              <label htmlFor="email">Email</label>
+              <Field type="email" name="email" id="email" />
+              <ErrorMessage name="email" component="div" className={css.error} />
+            </div>
+            <div className={css.formGroup}>
+              <label htmlFor="password">Password</label>
+              <Field type="password" name="password" id="password" />
+              <ErrorMessage name="password" component="div" className={css.error} />
+            </div>
+            {isSignup && (
+              <div className={css.formGroup}>
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <Field type="password" name="confirmPassword" id="confirmPassword" />
+                <ErrorMessage name="confirmPassword" component="div" className={css.error} />
+              </div>
+            )}
+            <button type="submit" disabled={isSubmitting}>
+              {isSignup ? "Sign Up" : "Sign In"}
+            </button>
+          </Form>
+        )}
+      </Formik>
       <div className={css.navigation}>
         {isSignup ? (
           <p>
