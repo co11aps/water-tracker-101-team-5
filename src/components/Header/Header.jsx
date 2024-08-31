3fe0f962e1ac69e9a5a8b370f04eab860173363e
Kris-tina30 <kristina.pichkar.15@gmail.com>
2024-08-31 22:55:03 +0200

    styles header new logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import css from "./header.module.css";
 import Icon from "../Icon/Icon";
-import logo from "../../images/logo/icon-logo.svg";
+import logo from "../../images/logo/Logo.svg";
 
 const Header = () => {
   return (
     <div className={css.headerContainer}>
       <header className={css.header}>
-        <a href="" lang="en" className="logo">
-          Tracker of water
+        <a href="./" lang="en" className={css.logo}>
           <img src={logo} alt="logo" />
         </a>
-        <nav className="header-nav">
-          <button className="header-button" type="button">
+        <nav className={css.headerNav}>
+          <button className={css.headerButton} type="button">
             Sign in
-            {/* <Icon id={}/> */}
+            <Icon id="user" className={css.buttonSvg} />
           </button>
         </nav>
       </header>
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,8 +1,10 @@
+import Header from "../../components/Header/Header";
 import Main from "../../components/Main/Main";
 
 export default function WelcomePage() {
   return (
     <div>
+      <Header />
       <Main />
     </div>
   );
