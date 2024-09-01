1f57a33ad6865b5d35687ea9c4ba83afc4b2bcf4
co11aps <143988142+co11aps@users.noreply.github.com>
2024-09-01 12:53:56 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,15 @@ import SigninPage from "./pages/SigninPage/SigninPage";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
 import HomePage from "./pages/HomePage/HomePage";
-import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/signin" element={<SigninPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/" element={<WelcomePage />} />
-        <Route
-          path="/"
-          element={
-            <>
-              <ColorSchemeToggle />
-              <h1>Water Tracker</h1>
-              <div>
-                <Link to="/signup">Sign Up</Link>
-              </div>
-              <Calendar />
-            </>
-          }
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/signin" element={<SigninPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/welcome" element={<WelcomePage />} />
+      <Route path="/home" element={<HomePage />} />
+    </Routes>
   );
 }
 
