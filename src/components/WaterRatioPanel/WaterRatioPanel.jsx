f17b9e1ce82b6171e820e2004480dc122db58df4
anastasiyaaa0 <anastasiiiia98@gmail.com>
2024-09-01 16:38:43 +0300

    add WaterRatioPanel component

--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -1,5 +1,42 @@
+import { useSelector } from "react-redux";
+
 import css from "./WaterRatioPanel.module.css";
+import AddWaterBtn from "../AddWaterBtn/AddWaterBtn";
 
 export default function WaterRatioPanel() {
-  return <div></div>;
+  // const { drankToday } = useSelector();
+  const drankTodayValue = 15;
+
+  return (
+    <>
+      <h2 className={css.header}>Today</h2>
+
+      <div className={css.container}>
+        <div className={css.progressBarContainer}>
+          <div className={css.progressBar}>
+            <div
+              className={css.progressBarCompleted}
+              style={{ width: `${drankTodayValue}%` }}
+            >
+              <div className={css.marker}>
+                <p className={css.percentageValue}>{drankTodayValue}%</p>
+              </div>
+            </div>
+          </div>
+          <div className={css.scale}>
+            <div className={css.tick}>
+              <span className={css.tickText}>0%</span>
+            </div>
+            <div className={css.tick}>
+              <span className={css.boldTickText}>50%</span>
+            </div>
+            <div className={css.tick}>
+              <span className={css.tickText}>100%</span>
+            </div>
+          </div>
+        </div>
+        <AddWaterBtn />
+      </div>
+    </>
+  );
 }
