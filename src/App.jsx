ff99e87dc335c2b5daac12fef42c209f338dafdc
co11aps <143988142+co11aps@users.noreply.github.com>
2024-09-02 01:26:02 +0800

    Revert "router"

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,19 @@
 import "./App.css";
-import ColorSchemeToggle from "./components/ColorSchemeToggler/ColorSchemeToggler";
-import Calendar from "./components/Calendar/Calendar";
+import { Route, Routes } from "react-router-dom";
+import SigninPage from "./pages/SigninPage/SigninPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
+import HomePage from "./pages/HomePage/HomePage";
 
-
-
-import { useEffect, lazy } from 'react';
-import { useDispatch,useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import Layout from "./components/Layout/Layout";
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute";
-import { refreshUser } from "./redux/auth/operations";
-import { selectIsRefreshing } from "./redux/auth/selectors";
-import Loader from "./components/Loader/Loader";
-import { Suspense } from "react";
-
-const App = () => {
-  const dispatch = useDispatch();
-  const { isRefreshing } = useSelector(selectIsRefreshing);
-
-  useEffect(() => {
-    dispatch(refreshUser());
-  }, [dispatch]);
-
+function App() {
   return (
-    <>
-      <Layout>
-        {isRefreshing ? (
-          <b>Refreshing user...</b>
-        ) : (
-          <Suspense fallback={<Loader />}>
-            <Routes>
-              <Route path="/" element={<WelcomePage />} />
-             
-              <Route
-                path="/signin"
-                element={
-                  <RestrictedRoute>
-                    <SigninPage />
-                  </RestrictedRoute>
-                }
-                />
-                
-                <Route
-                path="/signup"
-                element={
-                  <RestrictedRoute>
-                    <SignupPage/>
-                  </RestrictedRoute>
-                }
-              />
-              <Route
-                path="/home"
-                element={
-                  <PrivateRoute>
-                    <HomePage />
-                  </PrivateRoute>
-                }
-              />
-              
-             
-            </Routes>
-          </Suspense>
-        )}
-      </Layout>
-    </>
+    <Routes>
+      <Route path="/signin" element={<SigninPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/welcome" element={<WelcomePage />} />
+      <Route path="/home" element={<HomePage />} />
+    </Routes>
   );
 }
-
-
-
-
 
 export default App;
