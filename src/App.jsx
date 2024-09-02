0f10a511c65ef02f1930393cb4add6b5b10cff12
vika59216 <viktoriiadupreez@gmail.com>
2024-09-02 10:52:07 +0200

    routers

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,77 @@ import SignupPage from "./pages/SignupPage/SignupPage";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
 import HomePage from "./pages/HomePage/HomePage";
 
-function App() {
+
+
+
+
+import { useEffect, lazy } from 'react';
+import { useDispatch,useSelector } from 'react-redux';
+
+import Layout from "./components/Layout/Layout";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute";
+import { refreshUser } from "./redux/auth/operations";
+import { selectIsRefreshing } from "./redux/auth/selectors";
+import Loader from "./components/Loader/Loader";
+import { Suspense } from "react";
+
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useSelector(selectIsRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
-    <Routes>
-      <Route path="/signin" element={<SigninPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/welcome" element={<WelcomePage />} />
-      <Route path="/home" element={<HomePage />} />
-    </Routes>
+    <>
+      <Layout>
+        {isRefreshing ? (
+          <b>Refreshing user...</b>
+        ) : (
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<WelcomePage />} />
+             
+              <Route
+                path="/signin"
+                element={
+                  <RestrictedRoute>
+                    <SigninPage />
+                  </RestrictedRoute>
+                }
+                />
+                
+                <Route
+                path="/signup"
+                element={
+                  <RestrictedRoute>
+                    <SignupPage/>
+                  </RestrictedRoute>
+                }
+              />
+              <Route
+                path="/home"
+                element={
+                  <PrivateRoute>
+                    <HomePage />
+                  </PrivateRoute>
+                }
+              />
+              
+             
+            </Routes>
+          </Suspense>
+        )}
+      </Layout>
+    </>
   );
 }
+
+
+
+
 
 export default App;
