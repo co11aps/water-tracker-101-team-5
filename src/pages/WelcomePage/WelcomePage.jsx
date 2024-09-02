53301953362681a9fab3e958b6744c153c057b85
co11aps <143988142+co11aps@users.noreply.github.com>
2024-09-02 19:29:00 +0800

    Update WelcomePage.jsx

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,12 +1,9 @@
-import Header from "../../components/Header/Header";
 import Main from "../../components/Main/Main";
 import css from "./WelcomePage.module.css";
-
 
 export default function WelcomePage() {
   return (
     <div className={css.backgroundContainer}>
-      <Header />
       <Main />
     </div>
   );
