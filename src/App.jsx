633f228ec1f39ec63be070a63a57d914c14a17c2
co11aps <143988142+co11aps@users.noreply.github.com>
2024-09-03 11:57:03 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute";
 import { refreshUser } from "./redux/auth/operations";
 import { selectIsRefreshing } from "./redux/auth/selectors";
-import Loader from "./components/Loader/Loader";
-import { Suspense } from "react";
+import { Navigate } from "react-router-dom";
 
 const SigninPage = lazy(() => import("./pages/SigninPage/SigninPage"));
 const SignupPage = lazy(() => import("./pages/SignupPage/SignupPage"));
@@ -26,39 +25,36 @@ function App() {
   return isRefreshing ? (
     <b>Refreshing user...</b>
   ) : (
-      <Layout>
-        
-  
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route
-            path="/signin"
-            element={
-              <RestrictedRoute>
-                <SigninPage />
-              </RestrictedRoute>
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              <RestrictedRoute>
-                <SignupPage />
-              </RestrictedRoute>
-            }
-          />
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate to="/welcome" replace />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route
+          path="/signin"
+          element={
+            <RestrictedRoute>
+              <SigninPage />
+            </RestrictedRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <RestrictedRoute>
+              <SignupPage />
+            </RestrictedRoute>
+          }
+        />
 
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <HomePage />
-              </PrivateRoute>
-            }
-          />
-          </Routes>
-          
-     
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </Layout>
   );
 }
