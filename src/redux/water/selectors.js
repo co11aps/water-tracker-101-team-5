7489416daa4729e983de1e5d4c8887bd3baebe04
Natalie-Dor <varnata000@gmail.com>
2024-09-05 21:10:12 +0300

    list modal redux

--- src/redux/water/selectors.js
+++ src/redux/water/selectors.js
@@ -1,3 +1,9 @@
 export const selectDailyWater = (state) => state.water.dailyWater;
 
+export const selectDailyWaterIntakes = (state) =>
+  state.water.dailyWater.waterIntakes;
+
+// export const selectDailyWaterPercentage = (state) =>
+//   state.water.dailyWater.percentage;
+
 export const selectMonthlyWater = (state) => state.water.monthlyWater;
