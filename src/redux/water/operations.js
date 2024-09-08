3d7480f38b9674da2ee229698f2d48088f5eb2fe
co11aps <143988142+co11aps@users.noreply.github.com>
2024-09-08 12:46:16 +0800

    Update operations.js

--- src/redux/water/operations.js
+++ src/redux/water/operations.js
@@ -19,10 +19,8 @@ export const getMonthlyWater = createAsyncThunk(
     try {
       const formattedMonth = month.toString().padStart(2, "0"); // Ensure month is two digits
       const response = await axiosInstance.post("water/month", {
-        data: {
-          year: year.toString(), // Ensure year is a string
-          month: formattedMonth, // Send the formatted month
-        },
+        year: year.toString(), // Ensure year is a string
+        month: formattedMonth, // Send the formatted month
       });
       console.log("Request body:", {
         year: year.toString(),
